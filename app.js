f54593986dfec0deae2aaafeef112c9effb9cf20
Leonardo Javier Achear <leoachear@gmail.com>
2015-04-28 22:57:42 -0300

    Level 2.3

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 (function(){
 	var app = angular.module('gemStore', []);
-	var gem = {
-		name: 'Azurite',
-		price: 2.95,
-		canPurchase: false,
-    	soldOut: true
-	}
+
+	var gems =	[					
+					{
+						name: 'Azurite',
+						price: 2.95,
+						canPurchase: true,
+				    	soldOut: false
+					},
+					{
+						name: 'Bloodstone',
+						price: 5.95,
+						canPurchase: true,
+				    	soldOut: false
+					},
+					{
+						name: 'Zircon',
+						price: 3.95,
+						canPurchase: true,
+				    	soldOut: false
+					},
+				];
 
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
-
-
 })();
